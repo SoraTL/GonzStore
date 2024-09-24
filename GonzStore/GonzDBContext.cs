78d7e34a091cc7a336d4b4edc68f0852e73bf336
SoraTL <lamnguyen01476@gmail.com>
2024-09-24 12:45:17 +0700

    cm

---
+++ GonzStore/GonzDBContext.cs
@@ -0,0 +1,16 @@
+﻿using GonzStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GonzStore
+{
+    public class GonzDBContext : DbContext
+    {
+        public GonzDBContext() : base("metadata=res://*/Models.DatabaseGonzStore.csdl|res://*/Models.DatabaseGonzStore.ssdl|res://*/Models.DatabaseGonzStore.msl;provider=System.Data.SqlClient;provider connection string=&quot;data source=SORR\SQLEXPRESS;initial catalog=Gonz;integrated security=True;trustservercertificate=True;MultipleActiveResultSets=True;App=EntityFramework&quot;") 
+        { }
+        public DbSet<Customer> customers { get; set; }
+    }
+}
